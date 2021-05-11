18153302c5bbd4b40e1cb86f430263d4194a7be8
kuochiayu <84003059+kuochiayu@users.noreply.github.com>
2021-05-11 14:43:21 +0800

    Update 讀入隨機英文字串.c

--- 讀入隨機英文字串.c
+++ 讀入隨機英文字串.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 #define LEN 101
-#define idx 50000
+#define idx 10000
 
 int main() 
 {
@@ -13,7 +13,7 @@ int main()
     char text[LEN];
     char words[idx][LEN];
     FILE *fp = NULL;
-    fp = fopen("C:\\Users\\Lenovo\\Desktop\\c\\dataset2.txt","w");//在指定目录下创建.txt文件
+    fp = fopen("D:\\Users\\desktop\\sort\\dataset2.txt","a");//在指定目录下创建.txt文件
     memset(text, 1, LEN);
     for (int j=0; j<idx; j++) 
     {
@@ -28,7 +28,7 @@ int main()
     //text[LEN-1] = 0;
     for(int i=0; i<idx; i++)
     {
-        printf("%d: %s\n",i+1,words[i]);
+        printf("%d:\t%s\n", i+1, words[i]);
         fprintf(fp,"%s\n",words[i]); //把随机数写进文件
     }
     fclose(fp); //关闭文件
