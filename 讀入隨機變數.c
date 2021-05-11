c79d312fe985c3fa7aac7ec4ac6db8d3d24a7ebb
kuochiayu <84003059+kuochiayu@users.noreply.github.com>
2021-05-11 15:02:06 +0800

    Update 讀入隨機變數.c

--- 讀入隨機變數.c
+++ 讀入隨機變數.c
@@ -5,16 +5,16 @@
  
 int main(void)
 {
-	srand(time(NULL));//先种种子 
+	srand(time(NULL));//種種子
 	int i,j;
 	FILE *fp = NULL;
-    fp = fopen("C:\\Users\\Lenovo\\Desktop\\c\\dataset1.txt","w");//在指定目录下创建.txt文件
-	for(i = 0 ; i < 1000000 ; i ++) //产生10个100以内的随机数 
+    fp = fopen("C:\\Users\\Lenovo\\Desktop\\c\\dataset1.txt","w");//在指定目錄創建.txt檔案
+	for(i = 0 ; i < 1000000 ; i ++) //產生10000000個隨機數
 	{
 		j = rand()%100 ;
 		printf("j:%d\n",j);
-        fprintf(fp,"%d\n",j); //把随机数写进文件
+        	fprintf(fp,"%d\n",j); //寫進文件
     }
-    fclose(fp); //关闭文件
+    fclose(fp); //關閉文件
     return 0;
 }
